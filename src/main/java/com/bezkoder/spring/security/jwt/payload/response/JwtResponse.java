27068a5a33c7bced767a30e911a5b691dc343fae
Ming Jing <jingming.cn@gmail.com>
2023-05-22 01:52:05 +0800

    -

---
+++ src/main/java/com/bezkoder/spring/security/jwt/payload/response/JwtResponse.java
@@ -0,0 +1,84 @@
+package com.bezkoder.spring.security.jwt.payload.response;
+
+import java.util.List;
+
+public class JwtResponse {
+	private String token;
+	private String type = "Bearer";
+	private String refreshToken;
+	private Long id;
+	private String username;
+	private String email;
+	private String cellphone;
+	private final List<String> roles;
+
+	public JwtResponse(String accessToken,String refreshToken, Long id, String username, String email, String cellphone, List<String> roles) {
+		this.token = accessToken;
+		this.refreshToken = refreshToken;
+		this.id = id;
+		this.username = username;
+		this.email = email;
+		this.cellphone = cellphone;
+		this.roles = roles;
+	}
+
+	public String getAccessToken() {
+		return token;
+	}
+
+	public void setAccessToken(String accessToken) {
+		this.token = accessToken;
+	}
+
+	public String getTokenType() {
+		return type;
+	}
+
+	public void setTokenType(String tokenType) {
+		this.type = tokenType;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public List<String> getRoles() {
+		return roles;
+	}
+
+	public String getCellphone() {
+		return cellphone;
+	}
+
+	public void setCellphone(String cellphone) {
+		this.cellphone = cellphone;
+	}
+
+	public String getRefreshToken() {
+		return refreshToken;
+	}
+
+	public void setRefreshToken(String refreshToken) {
+		this.refreshToken = refreshToken;
+	}
+}
